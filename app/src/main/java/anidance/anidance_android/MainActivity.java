2345240b27bb1501a8ce30ec735ded96f4f837ce
nodgd <dcc1234@qq.com>
2018-07-10 02:55:22 +0800

    在主界面MainActivity里增加了打开刚加的两个Activity的按钮。

--- app/src/main/java/anidance/anidance_android/MainActivity.java
+++ app/src/main/java/anidance/anidance_android/MainActivity.java
@@ -1,13 +1,34 @@
 package anidance.anidance_android;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main);
+
+        //模式选择的按钮
+        findViewById(R.id.mode_selection_file_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, FileModeActivity.class));
+            }
+        });
+        findViewById(R.id.mode_selection_live_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LiveModeActivity.class));
+            }
+        });
     }
 }
