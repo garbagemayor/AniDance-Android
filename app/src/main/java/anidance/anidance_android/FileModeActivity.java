1e6f8e56901f0364e265c8d8c7fc6fb092166d7a
nodgd <dcc1234@qq.com>
2018-07-15 15:26:50 +0800

    选文件模式的选文件功能，获取到文件的Uri。

--- app/src/main/java/anidance/anidance_android/FileModeActivity.java
+++ app/src/main/java/anidance/anidance_android/FileModeActivity.java
@@ -1,19 +1,30 @@
 package anidance.anidance_android;
 
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.database.Cursor;
+import android.media.MediaPlayer;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
 
 public class FileModeActivity extends AppCompatActivity {
 
     public static String TAG = "FileModeActivity";
 
-
+    private Uri mFileUri = null;
+    MediaPlayer mMediaPlayer = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,15 +42,56 @@ public class FileModeActivity extends AppCompatActivity {
                 startActivityForResult(intent, 1);
             }
         });
+
+        //试听按钮
+        findViewById(R.id.file_mode_listen_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mFileUri != null) {
+                    mMediaPlayer = new MediaPlayer();
+                    try {
+                        mMediaPlayer.setDataSource(FileModeActivity.this, mFileUri);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    mMediaPlayer.prepareAsync();
+                    mMediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                        @Override
+                        public void onPrepared(MediaPlayer mp) {
+                            mMediaPlayer.start();
+                        }
+                    });
+                }
+            }
+        });
     }
 
+    //浏览文件返回时
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == Activity.RESULT_OK) {
+            Log.d(TAG, "requestCode = " + resultCode);
             if (requestCode == 1) {
-                Uri uri = data.getData();
-                Toast.makeText(this, "文件路径："+uri.getPath().toString(), Toast.LENGTH_SHORT).show();
+                mFileUri = data.getData();
+                String fileDisplayName = getDisplayNameFromUri(FileModeActivity.this, mFileUri);
+                ((TextView) FileModeActivity.this.findViewById(R.id.file_mode_path_tv)).setText(fileDisplayName);
             }
         }
     }
 
+    //从Uri获取不带路径的文件名
+    private static String getDisplayNameFromUri(Context context, Uri uri) {
+        if (uri == null || uri.getScheme() == null) {
+            return null;
+        } else if (uri.getScheme().equals(ContentResolver.SCHEME_FILE)) {
+            return uri.getLastPathSegment();
+        }
+        Cursor cursor = context.getContentResolver().query( uri, new String[] {MediaStore.MediaColumns.DISPLAY_NAME}, null, null, null );
+        if (cursor == null) {
+            return null;
+        }
+        cursor.moveToFirst();
+        int columnIndex = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DISPLAY_NAME);
+        return cursor.getString(columnIndex);
+    }
 }
--- app/src/main/java/anidance/anidance_android/MainActivity.java
+++ app/src/main/java/anidance/anidance_android/MainActivity.java
@@ -1,11 +1,20 @@
 package anidance.anidance_android;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
+import android.widget.Toast;
+
+import java.security.Permission;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main);
+
+        //检查App是否有足够的权限
+        requestAllPermissions();
 
         //模式选择的按钮
         findViewById(R.id.mode_selection_file_btn).setOnClickListener(new View.OnClickListener() {
@@ -30,5 +42,39 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, LiveModeActivity.class));
             }
         });
+    }
+
+    private void requestAllPermissions() {
+        String[] permissions = {
+                Manifest.permission.INTERNET,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.RECORD_AUDIO,
+        };
+        Log.d(TAG, "申请?");
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "申请!");
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                Toast.makeText(MainActivity.this, "向用户解释", Toast.LENGTH_SHORT).show();
+            } else {
+                ActivityCompat.requestPermissions(MainActivity.this, permissions, 1);
+                Toast.makeText(MainActivity.this, "申请", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        Toast.makeText(MainActivity.this, "申请结果：", Toast.LENGTH_SHORT).show();
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            for (int i = 0; i < permissions.length; i++) {
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "" + "权限" + permissions[i] + "申请成功", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "" + "权限" + permissions[i] + "申请失败", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
     }
 }
